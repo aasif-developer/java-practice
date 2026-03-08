5e07e9973fee330e80d3e40179171731cd72fae6
MOHAMMED AASIF <aasifmmd12345@gmail.com>
2026-03-08 23:16:06 +0530

    Modify addNumbers function to accept three parameters

--- UserDefinedFn.java
+++ UserDefinedFn.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class UserDefinedFunctionExample {
 
     // User-defined function
-    public static int addNumbers(int a, int b) {
-        int sum = a + b;
+    public static int addNumbers(int a, int b , int c) {
+        int sum = a + b + c;
         return sum;
     }
 
@@ -18,8 +18,10 @@ public class UserDefinedFunctionExample {
         System.out.print("Enter second number: ");
         int num2 = sc.nextInt();
 
+        System.out.println("Enter Third Number : ")
+
         // Calling the user-defined function
-        int result = addNumbers(num1, num2);
+        int result = addNumbers(num1, num2 , num3);
 
         System.out.println("Sum = " + result);
     }
