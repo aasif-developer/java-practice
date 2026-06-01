1d672efd07e6eceb27b209139cdedeba32a3aea1
aasif-create <aasifmmd12345@gmail.com>
2026-06-01 15:51:59 +0530

    UPDATED

--- Revise.java
+++ Revise.java
@@ -1,4 +1,14 @@
 import java.util.Scanner;
+class Student
+{
+    int roll;
+    String name;
+    void display()
+    {
+        System.out.println("Roll number: "+roll);
+        System.out.println("Name: "+name);
+    }
+}
 public class Revise
 {
     public static void main(String[] args)
@@ -14,6 +24,10 @@ public class Revise
         else
         {
             System.out.println("Odd number");
-        }   
+        } 
+        Student s1 = new Student();
+        s1.roll = 101;
+        s1.name="AASIF";
+        s1.display();
     }
 }
